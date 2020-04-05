00b550a2ce68df1a382e6019b7cebb0a8f1790ce
Nazmul Hoque <coder.nazmul@gmail.com>
2020-04-05 20:36:16 +0600

    new api created to insert all food items to database

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ app.use(cors())
 const uri = process.env.DB_PATH;
 let client = new MongoClient(uri, { useNewUrlParser: true });
 
+app.post('/addAllFoods', (req, res)=> {
+    const foods = req.body;
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+    const collection = client.db("hotOnion").collection("foods");
+    collection.insert(foods, (error, result) => {
+        if(error) {
+            console.log(error);
+            res.status(500).send({message:error});
+        }
+        else {
+            res.send(result.ops[0]);
+        }
+    })
+    client.close();
+    });
+});
+
 app.post('/addFood', (req, res) => {
     const food = req.body;
     client = new MongoClient(uri, { useNewUrlParser: true });

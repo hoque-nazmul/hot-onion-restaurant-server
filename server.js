4ac5721a786c70cae38561c0df84901d1bb2519f
Nazmul Hoque <coder.nazmul@gmail.com>
2020-04-05 20:02:45 +0600

    important api created for using client side

--- server.js
+++ server.js
@@ -2,12 +2,67 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient;
 
 // App
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
+const uri = process.env.DB_PATH;
+let client = new MongoClient(uri, { useNewUrlParser: true });
+
+app.post('/addFood', (req, res) => {
+    const food = req.body;
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+    const collection = client.db("hotOnion").collection("foods");
+    collection.insertOne(food, (error, result) => {
+        if(error) {
+            console.log(error);
+            res.status(500).send({message:error});
+        }
+        else {
+            res.send(result.ops[0]);
+        }
+    })
+    client.close();
+    });
+});
+
+app.get('/getFoods', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+    const collection = client.db("hotOnion").collection("foods");
+    collection.find().toArray((error, documents) => {
+        if(error) {
+            console.log(error);
+            res.status(500).send({message:error});
+        }
+        else {
+            res.send(documents);
+        }
+    })
+    client.close();
+    });
+});
+
+app.get('/getFood', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+    const collection = client.db("hotOnion").collection("foods");
+    collection.find({name : "Nzmul Hoque" }).toArray((error, result) => {
+        if(error) {
+            console.log(error);
+            res.status(500).send({message:error});
+        }
+        else {
+            res.send(result);
+        }
+    })
+    client.close();
+    });
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -18,7 +73,7 @@ function formatDate(date) {
     var minutes = date.getMinutes();
     var ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
+    hours = hours ? hours : 12;
     minutes = minutes < 10 ? '0'+minutes : minutes;
     var strTime = hours + ':' + minutes + ' ' + ampm;
     return strTime;
